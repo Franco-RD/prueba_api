c1f75f333b542ace4cbd90c45b77e1a71714ef13
Franco-RD <franco.romano.data@gmail.com>
2023-12-11 18:51:31 -0300

    Primera version de tasa_cambios con bucle

--- appcoints/tasa_cambios.py
+++ appcoints/tasa_cambios.py
@@ -1,29 +1,27 @@
 import requests 
+#from .utils import *  #En este import se pone solo . antes de utils porque el archivo en el que estoy esta a la misma altura que la carpeta utils. 
 
-apikey = '046BC141-EE9A-429C-AB7A-14953FA91D43'
+APIKEY = '046BC141-EE9A-429C-AB7A-14953FA91D43'
 moneda_cripto = input("Ingrese una criptomoneda conocida: ").upper()
 
-#Invocando al metodo get con url especifica
-url = f'https://rest.coinapi.io/v1/exchangerate/{moneda_cripto}/EUR?apikey={apikey}'
 
-r = requests.get(url)  #Hace un get a la url y guarda lo obtenido en la variable r
+while moneda_cripto != '' and moneda_cripto.isalpha():  #.isalpha() comprueba que todos los caracteres de un str sean letras. Da True si lo son. 
 
-"""
-print("Codigo http de respuesta: ", r.status_code)  #Status code da el codigo de respuesta (200, 404, etc.)
-print("Cabecera: ", r.headers['content-type'])  #r.headers obtiene la cabecera 
-print("Encoding: ", r.encoding)  #Obtiene el encodeo
-print("Respuesta en str: ", r.text)  #Obtiene lo del get en formato texto
-print("Respuesta en JSON: ", r.json())  #Obtiene lo del get en formato JSON, y siempre va con () porque es un metodo. Para py los JSON son como diccionarios
-print(r.json()['time'])
-"""
+    #Invocando al metodo get con url especifica
+    url = f'https://rest.coinapi.io/v1/exchangerate/{moneda_cripto}/EUR?apikey={APIKEY}'
 
-#Ej 1, como capturar el rate.
-respuesta = r.json()  #Guardo el JSON en una variable que queda como diccionario
-#print("Rate: ", respuesta['rate'])
+    r = requests.get(url)  #Hace un get a la url y guarda lo obtenido en la variable r
+
+    #Ej 1, como capturar el rate.
+    respuesta = r.json()  #Guardo el JSON en una variable que queda como diccionario
+    #print("Rate: ", respuesta['rate'])
 
 
-#Ej 2, como capturo errores de peticion http.
-if r.status_code == 200:
-    print("Rate: ", respuesta['rate'])  #Cuando todo funciona el JSON da un diccionario con unas claves particulares (diccionario respuesta en pruebas)
-else:
-    print("Error: ", respuesta['error'])  #Cuando da un error porque la url esta mal, da otro diccionario (tambien en pruebas), que la key que tiene se llama 'error'. 
+    #Ej 2, como capturo errores de peticion http.
+    if r.status_code == 200:
+        print("Rate: {:.2f}€".format(respuesta['rate']))  #Cuando todo funciona el JSON da un diccionario con unas claves particulares (diccionario respuesta en pruebas)
+        break
+    else:
+        print("Error: ", respuesta['error'])  #Cuando da un error porque la url esta mal, da otro diccionario (tambien en pruebas), que la key que tiene se llama 'error'. 
+
+    moneda_cripto = input("Ingrese una criptomoneda conocida: ").upper()
--- pruebas.py
+++ pruebas.py
@@ -8,3 +8,12 @@ respuesta = {
 error = {
   "error": "You didn\u0027t specify API key or it is incorrectly formatted. You should do it in: query string parameter \u0060apikey\u0060,  in http header named \u0060X-CoinAPI-Key\u0060,  in http header named \u0060Authorization\u0060 or  as part of URL /apikey-YOUR-API-KEY/"
 }
+
+"""
+print("Codigo http de respuesta: ", r.status_code)  #Status code da el codigo de respuesta (200, 404, etc.)
+print("Cabecera: ", r.headers['content-type'])  #r.headers obtiene la cabecera 
+print("Encoding: ", r.encoding)  #Obtiene el encodeo
+print("Respuesta en str: ", r.text)  #Obtiene lo del get en formato texto
+print("Respuesta en JSON: ", r.json())  #Obtiene lo del get en formato JSON, y siempre va con () porque es un metodo. Para py los JSON son como diccionarios
+print(r.json()['time'])
+"""
